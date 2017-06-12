37475932e355accd8d9607160a1acf1ee237fabc
juanbanchero <juan.banche@gmail.com>
2017-06-11 23:56:09 -0300

    Correccion

--- TP Logico.pl
+++ TP Logico.pl
@@ -9,7 +9,7 @@ trabajaPara(marsellus,vincent).
 trabajaPara(marsellus,jules).
 trabajaPara(marsellus,winston).
 
-%%%%      Punto 1     %%%%
+%%%%      Punto 1     %%%% 
 
 saleCon(UnaPersona,OtraPersona) :- pareja(UnaPersona,OtraPersona).
 saleCon(UnaPersona,OtraPersona) :- pareja(OtraPersona,UnaPersona).
@@ -20,3 +20,5 @@ pareja(bernardo,bianca).
 pareja(bernardo,charo).
 
 %% Como no se si Bianca es pareja de Demóstenes, no puedo afirmar algo que no sea cierto. No tiene sentido escribir algo que no es cierto en Prolog
+
+%%%% Punto 3 %%%%
