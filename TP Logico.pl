703008fc7782142f1cdbf877086563cfedbc46ab
juanbanchero <juan.banche@gmail.com>
2017-06-12 17:02:32 -0300

    Terminada la primera entrega

--- TP Logico.pl
+++ TP Logico.pl
@@ -1,24 +1,35 @@
 %%%%% PRIMERA ENTREGA %%%%%
 
 % pareja(Persona,Persona)
+%% se agrega el punto 2 %%
+%% Como no se si Bianca es pareja de Demóstenes, no puedo afirmar algo que no sea cierto. No tiene sentido escribir algo que no es cierto en Prolog
 pareja(marsellus,mia).
 pareja(pumkin,honeyBunny).
+pareja(bernardo,bianca).
+pareja(bernardo,charo).
 
 % trabajaPara(Empleador,Empleado)
+%% se agrega el punto 3 %%
 trabajaPara(marsellus,vincent).
 trabajaPara(marsellus,jules).
 trabajaPara(marsellus,winston).
+trabajaPara(Empleador,bernardo) :- trabajaPara(marsellus,Empleador), Empleador \= jules.
+trabajaPara(Empleador,george) :- saleCon(bernardo,Empleador).
+
 
 %%%%      Punto 1     %%%% 
 
 saleCon(UnaPersona,OtraPersona) :- pareja(UnaPersona,OtraPersona).
 saleCon(UnaPersona,OtraPersona) :- pareja(OtraPersona,UnaPersona).
 
-%%%%      Punto 2     %%%%
+%%%%      Punto 4     %%%% 
+esFiel(Persona) :- saleCon(Persona,_),not(saleConMasDeUno(Persona)).
 
-pareja(bernardo,bianca).
-pareja(bernardo,charo).
+saleConMasDeUno(Persona) :- saleCon(Persona,OtraPersona), saleCon(Persona,OtraPersonaMas), OtraPersona \= OtraPersonaMas.
 
-%% Como no se si Bianca es pareja de Demóstenes, no puedo afirmar algo que no sea cierto. No tiene sentido escribir algo que no es cierto en Prolog
+%%%%      Punto 5     %%%% 
+acataOrden(Jefe,Empleado) :- trabajaPara(Jefe,Empleado).
+acataOrden(Jefe,Empleado) :- trabajaPara(Jefe,Empleador),acataOrden(Empleador,Empleado).
 
-%%%% Punto 3 %%%%
+
+
